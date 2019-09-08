2de886ad339960fbe3a9e82f4ed82de47dc51593
Gabor Szarnyas <szarnyasg@gmail.com>
2019-09-08 22:24:28 +0200

    Reformat code, fix line endings

--- hu.bme.mit.csi.base/src/test/java/hu/bme/mit/critlab/base/FruitTest.java
+++ hu.bme.mit.csi.base/src/test/java/hu/bme/mit/critlab/base/FruitTest.java
@@ -5,14 +5,14 @@ import java.util.concurrent.ExecutionException;
 import org.junit.Test;
 
 public class FruitTest extends TestBase {
-	
-	@Test
-	public void fruitTest() throws ExecutionException {
-		doSimpleTest(9.0 / 11.0, 
-				"Banana", 
-				"Ananas", 
-				2, false);
-		
-	}
+
+    @Test
+    public void fruitTest() throws ExecutionException {
+        doSimpleTest(9.0 / 11.0,
+                "Banana",
+                "Ananas",
+                2, false);
+
+    }
 
 }
